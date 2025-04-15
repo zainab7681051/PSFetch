174d3fe32ac4038d284fa227284234ca552b7fad
zainab7681051 <zainab76810510@gmail.com>
2025-04-15 12:14:49 +0300

    commit

--- PSFetch.ps1
+++ PSFetch.ps1
@@ -10,11 +10,10 @@ function Invoke-PSFetch {
       '-..-'|         ||         |n
       '-..-'|         ||         |n
       '-..-'|         ||         |n
-      '-..-'|___...---''---...___|n 
+      '-..-'|___...---''---...___|n
 
-"@ -split ""
+"@ -split "n"
 
-    # System Information
     $os = Get-CimInstance Win32_OperatingSystem
     $cpu = Get-CimInstance Win32_Processor
     $memory = Get-CimInstance Win32_PhysicalMemory
@@ -33,22 +32,17 @@ function Invoke-PSFetch {
         "PowerShell: $($PSVersionTable.PSVersion)"
     )
 
-    # Display logo and info
     $color = "Blue"
     $info_index=0
-    foreach ($char in $logo) {
-      if(-not($char -ceq "n")){
-        Write-Host -NoNewline -ForegroundColor $color $char
-      }
-      else{
-        if($info_index -lt $info.Count){
-          Write-Host -NoNewline ((" " * 3) + ($info[$info_index++])) 
-        }
+    foreach ($line in $logo) {
+      Write-Host -NoNewline -ForegroundColor $color $line
+      if($info_index -lt $info.Count){
+        Write-Host -NoNewline ((" " * 3) + ($info[$info_index++])) 
       }     
     }
 }
 
-Invoke-PSFetch
+# Invoke-PSFetch
 
 <#if (($Host.Name -eq 'ConsoleHost' -or $Host.Name -like '*Terminal*') -and -not [System.Console]::IsOutputRedirected) {
     Invoke-PSFetch
