292e216c50b8505ff1464795d84e4b7b2de1aaa5
zainab7681051 <zainab76810510@gmail.com>
2025-04-16 12:26:58 +0300

    commit

--- PSFetch.ps1
+++ PSFetch.ps1
@@ -1,32 +1,32 @@
 function Invoke-PSFetch {
 
   function Print-NoNewLine{
-    param($Line, $Color)
+    param($Text, $Color)
     if($Color){
-      return Write-Host -NoNewline "$($Color) $Line $($PSStyle.Reset)"
+      return Write-Host -NoNewline "$($Color) $Text $($PSStyle.Reset)"
     }
-    return Write-Host -NoNewline $Line
+    return Write-Host -NoNewline $Text
   }
 
   $logo=@"
-                         ....iiilll
-                 ....iilllllllllllllN
-     ....iillll  lllllllllllllllllllN
- iillllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
-                                    N
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- llllllllllllll  lllllllllllllllllllN
- ''llllllllllll  lllllllllllllllllllN
-       ''''llll  ''lllllllllllllllllN
-                      ''''''^^^^llllN
+               cc        ....iiilllN
+               cc....iilllllllllllllN
+     ....iillllcclllllllllllllllllllN
+ iillllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+               cc                   N
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ llllllllllllllcclllllllllllllllllllN
+ ''llllllllllllcclllllllllllllllllllN
+       ''''llllcc''lllllllllllllllllN
+               cc     ''''''^^^^llllN
 "@ -split "N"
 
     #HARDWARE
@@ -35,6 +35,9 @@ function Invoke-PSFetch {
     $os = Get-CimInstance Win32_OperatingSystem
     #CPU
     $cpu = Get-CimInstance Win32_Processor | Select-Object Name, MaxClockSpeed
+    #GRAPHICS CARD
+    $videoController = Get-CimInstance Win32_VideoController | Select-Object Caption, CurrentHorizontalResolution, CurrentVerticalResolution
+    $gCard = $videoController.Caption
     #MEMORY [RAM]
     $memory = Get-CimInstance Win32_PhysicalMemory
     $totalMemory = ($memory | Measure-Object -Property Capacity -Sum).Sum / 1GB
@@ -45,40 +48,52 @@ function Invoke-PSFetch {
     $freeStorage = ($storage.SizeRemaining)/1GB
     $totalStorage = ($storage.size)/1GB
     $usedStorage = $totalStorage - $freeStorage
-    #additional
+    #ADDITIONAL
+    $resolution = "$($videoController.CurrentHorizontalResolution) x $($videoController.CurrentVerticalResolution)"
     $uptime = (Get-Date) - $os.LastBootUpTime
 
     $info = @(
         "PC: $($computer.Manufacturer) [$($computer.Model)]"
         "OS: $($os.Caption) [$($os.Version)]"
         "Hostname: $($env:COMPUTERNAME)"
-        "Uptime: $($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m"
         "CPU: $($cpu.Name) [$(($cpu.MaxClockSpeed)/1000)GHz]"
+        "Grahics card: $($gCard)"
+        "Resolution: $($resolution)"
         "Memory [RAM]: {0:N1}GB / {1:N1}GB" -f $usedMemory, $totalMemory
         "Storage [C]: {0:N1}GB / {1:N1}GB" -f $usedStorage, $totalStorage
         "PowerShell: $($PSVersionTable.PSVersion)"
+        "Uptime: $($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m"
     )
 
     $colors = @{
-      blue = $PSStyle.Foreground.FromRgb(0x0000FF) 
-      green = $PSStyle.Foreground.FromRgb(0x00FF00)
-      yellow = $PSStyle.Foreground.FromRgb(0xFFFF00) 
-      red = $PSStyle.Foreground.FromRgb(0xFF0000) 
+      blue = $PSStyle.Foreground.FromRgb(0x00aedb) 
+      green = $PSStyle.Foreground.FromRgb(0x00b159)
+      yellow = $PSStyle.Foreground.FromRgb(0xffc425) 
+      red = $PSStyle.Foreground.FromRgb(0xd11141) 
     } 
 
-    $line_num = 0
+    $info_indx = 0
+    $line_indx = 0
     foreach ($line in $logo) {
-      Print-NoNewLine -Line $line -Color $colors.blue
+      $lineParts = $line -split "cc", 2
 
-      if($line_num -lt $info.Count){
-        $text = $info[$line_num++]
+      if(($line_indx++) -ge ($logo.Count)/2){
+        Print-NoNewLine -Text $lineParts[0] -Color $colors.blue
+        Print-NoNewLine -Text $lineParts[1] -Color $colors.yellow
+      }
+      else {
+        Print-NoNewLine -Text $lineParts[0] -Color $colors.red 
+        Print-NoNewLine -Text $lineParts[1] -Color $colors.green
+      }
+
+      if($info_indx -lt $info.Count){
+        $text = $info[$info_indx++]
         $parts = $text -split ":", 2
         $label = $parts[0] + ":"
         $value = $parts[1] 
 
-        Print-NoNewLine -Line (" " * 3)
-        Print-NoNewLine -Line $label -Color $colors.green
-        Print-NoNewLine -Line $value
+        Print-NoNewLine -Text $label -Color $colors.blue
+        Print-NoNewLine -Text $value
       }     
     }
 }
